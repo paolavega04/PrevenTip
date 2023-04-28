c85f01e17ff7d44828ae3a3b7e571ef51894bad2
Salette Noemi Villalobos <A01246619@tec.mx>
2023-04-28 04:56:09 -0600

    Normal
    Sin cambios en el botón - Con carpetas

--- preventip/src/app/app.module.ts
+++ preventip/src/app/app.module.ts
@@ -1,56 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NoopAnimationsModule } from "@angular/platform-browser/animations";
-
-
-import { NavbarComponent } from './shared/navbar/navbar.component';
-import { LoginComponent } from './views/login/login.component';
 import { LoginModule } from './views/login/login.module';
 import { DashboardModule } from './views/dashboard/dashboard.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AngularMaterialsModule } from './angular-materials/angular-materials.module';
 import { SharedModule } from './shared/shared.module';
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatToolbarModule } from "@angular/material/toolbar";
-
-import { AuthInterceptorService } from "./services/auth-interceptor.service";
-
-
+import { MatToolbar } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    LoginComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    MatToolbarModule,
+    LoginModule,
+    DashboardModule,
     BrowserAnimationsModule,
     AngularMaterialsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatIconModule,
-    MatInputModule,
-    MatListModule,
-    ReactiveFormsModule,
-    HttpClientModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
